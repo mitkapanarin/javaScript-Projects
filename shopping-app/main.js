31117ff91b60a4c2455d5751196b7516c24db862
Mitka Panarin <mitka_j@yahoo.com>
2023-02-15 23:20:14 +0100

    new

--- shopping-app/main.js
+++ shopping-app/main.js
@@ -1,96 +1,115 @@
-const shop = document.getElementById("card");
+const shop = document.getElementById("shop");
 //console.log(shop)
 
-const dataShop = [
+let dataShop = [
   {
-    id: "ab",
+    id: 1,
     image: "assets/blouse.jpg",
     name: "Women shirt",
     material: "cotton",
     collor: "blue",
-    price: "$15.00",
+    price: 15.0,
+    quantity: 0,
   },
   {
-    id: "cd",
+    id: 2,
     image: "assets/casual.jpg",
     name: "Casual shirt",
     material: "cotton",
     collor: "blue",
-    price: "$15.00",
+    price: 15.0,
+    quantity: 0,
   },
   {
-    id: "ef",
-    image: "assets/sport.jpg",
+    id: 3,
+    image: "assets/sport.jpeg",
     name: "Sport shirt",
     material: "cotton",
     collor: "blue",
-    price: "$15.00",
+    price: 15.0,
+    quantity: 3,
   },
 ];
 
-let basket = [];
+let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 const allProducts = () => {
   return (shop.innerHTML = dataShop
     .map((d) => {
-      const { id, image, name, material, collor, price } = d;
+      const { id, image, name, material, collor, price, quantity } = d;
+      console.log(id)
       return `
-<div id=product-id-${id} class = "content">
-<div class="left">
-  <img width="100" src=${image} alt=""/>
-</div>
-<div class="right">
-  <h3>${name}</h3>
-  <p>${material}</p>
-  <p>${collor}</p>
-  <p>${price}</p>
-</div>
-</div>
-<div class="buttons">
-  <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-  <div id="cartAmount" class="cartAmount">0</div>
-  <i onclick="increment(${id})"class="bi bi-plus-lg"></i>
-</div>
+          <div id="product-id-${id}" class="content">
+          <div class="left">
+            <img width="100" src=${image} alt=""/>
+          </div>
+          <div class="right">
+            <h3>${name}</h3>
+            <p>${material}</p>
+            <p>${collor}</p>
+            <p>$ ${price}</p>
+          </div>
+          </div>
+          <div class="buttons">
+            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+            <div id="cartAmount" class="cartAmount">${quantity}</div>
+            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
 `;
     })
     .join(""));
 };
 
-console.log(allProducts);
-allProducts();
-
 let increment = (id) => {
-  let selectedItem = id;
-  let search = basket.find((d) => d.id === selectedItem.id);
+  dataShop = dataShop.map((item) =>
+    item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+  );
+  allProducts();
+  calculation()
 
-  if(search === undefined) {
-    basket.push({
-      id: selectedItem.id,
-      item: 1,
-    });
-  }
-  else{
-    search.item += 1;
-  }
- 
-  console.log(basket);
-  update();
+  console.log(dataShop)
 };
+
 
 let decrement = (id) => {
-  let selectedItem = id;
-  let search = basket.find((d) => d.id === selectedItem.id);
+  dataShop = dataShop.map((item) =>
+  item.id === id ? { ...item, quantity: item.quantity <=0 ? 0: item.quantity - 1 } : item
+);
+allProducts();
+calculation()
 
-  if(search.item === 0) return;
-  else{
-    search.item -= 1;
-  }
- 
-  console.log(basket);
-  update();
 };
 
-let update = () => {
-  console.log("the update function is running");
+let update = (id) => {};
+
+let calculation = () => {
+  const makeBill = dataShop.map(item=> item.quantity * item.price)
+  const sum = makeBill.reduce((a,b)=>a+b,0)
+  console.log(sum)
 };
+
+calculation();
+
+let TotalAmount = () => {
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((x) => {
+        let { id, item } = x;
+        let filterData = dataShop.find((x) => x.id === id);
+        return filterData.price * item;
+      })
+      .reduce((x, y) => x + y, 0);
+
+    return (label.innerHTML = `
+    <h2>Total Bill : $ ${amount}</h2>
+    <button class="checkout">Checkout</button>
+    <button onclick="clearCart()" class="removeAll">Clear Cart</button>
+    `);
+  } else return;
+};
+
+TotalAmount();
+
+
+allProducts();
 
